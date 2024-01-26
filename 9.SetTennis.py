d374efc98c11920d1bc317cc41bf7b4bf47f8f7e
eduardo608 <72846380+eduardo608@users.noreply.github.com.>
2024-01-25 21:52:03 -0500

    Ejercicio 9 Set de Tennis

--- 9.SetTennis.py
+++ 9.SetTennis.py
@@ -1,1 +1,20 @@
 #Desarrolle un programa que solucione el problema de Solarrabietas:
+
+A = int(input("Juegos ganados por A: "))
+B = int(input("Juegos ganados por B: "))
+
+if 0 < A <= 5 and 0 < B <= 5:
+    print("Aun no termina")
+elif 5 <= A <= 7 and 5 <= B <= 7:
+    if B==7:
+        print("Gano A")
+    elif B==7:
+        print("Gano B")
+    else:
+        print("Aun no termina")
+elif A == 6 and 0 <= B <5:
+    print("Gano A")
+elif B == 6 and 0 <=A <5:
+    print("Gano B")
+else:
+    print("Invalido")
