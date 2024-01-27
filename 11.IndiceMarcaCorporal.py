c23d4f1ac463a10ea1c54f24b4d9a7b6172a2ed1
Eduardo Villamizar <72846380+eduardo608@users.noreply.github.com>
2024-01-26 19:51:13 -0500

    Update 11.IndiceMarcaCorporal.py

--- 11.IndiceMarcaCorporal.py
+++ 11.IndiceMarcaCorporal.py
@@ -1,7 +1,7 @@
 #Escriba un programa que reciba como entrada la estatura, el peso y la edad de una persona, y le entregue su condición de riesgo
 
 import math
-imc = float(input("Ingrese su peso: ")) / (math.pow(float(input("Ingrese su altura: "))))
+imc = float(input("Ingrese su peso: ")) / math.pow(float(input("Ingrese su altura: ")), 2)
 edad = int(input("Ingrese su edad: "))
 if edad < 45 and imc < 22.0:
     print("bajo")
